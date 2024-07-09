47a34c8652f2459454f1c4f8c96cf74124f8f596
carloscuevas <ccuevasbalinas@gmail.com>
2024-07-09 18:04:49 +0200

    Add Security Rule

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -55,6 +55,17 @@ resource "azurerm_network_security_group" "ansg" {
   name                = var.network_security_group_name
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
+  security_rule {
+    name                       = "ccbcp2ansgsr"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_network_interface_security_group_association" "anisga" {
