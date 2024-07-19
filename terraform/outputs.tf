6aeba75e491c13c3b84034cbd0c778b0d1b2b7a2
carloscuevas <ccuevasbalinas@gmail.com>
2024-07-19 19:11:56 +0200

    Update terraform

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -10,3 +10,7 @@ output "container_registry_admin_password" {
   value     = azurerm_container_registry.acr.admin_password
   sensitive = true
 }
+
+output "virtual_machine_id" {
+  value = azurerm_linux_virtual_machine.avm.id
+}
